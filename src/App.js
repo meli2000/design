ceaee6d7f7200a36aff9e268037b1b071397cb55
meli2000 <melcaicer15@gmail.com>
2024-12-06 22:59:08 -0500

    cambiosresponsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import icon from './avatar.svg'
+import icono from './icono.png'
 import React, { useState } from 'react';
 
 
@@ -125,7 +125,7 @@ function App() {
     <div className='App-contenedor'>
       
       <h3 className='mainlabels'>Welcome again</h3>
-      <img src={icon} className='icono' width="100" height="100">
+      <img src={icono} className='icono' width="100" height="100">
       </img>
       <h3 className='mainlabels'> login </h3>
     </div>
